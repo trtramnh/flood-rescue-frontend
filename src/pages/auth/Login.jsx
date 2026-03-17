fb805701ab042157881b6fb38f06ce2a03d409a2
Trương Trần Anh Minh <minhtruong300605@gmail.com>
2026-03-17 11:38:00 +0700

    Rename CSS import from login.css to Login.css

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Header from "../../components/common/Header";
-import "./login.css";
+import "./Login.css";
 import { login } from "../../services/authService";
 
 const Dashboard = () => {
